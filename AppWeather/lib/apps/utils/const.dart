140a203c10465f591857ee7da1ce6b9da7c13224
LongLee239142 <long.ld195887@gmail.com>
2025-02-06 16:34:58 +0700

    update

--- AppWeather/lib/apps/utils/const.dart
+++ AppWeather/lib/apps/utils/const.dart
@@ -1,6 +1,6 @@
 const linkAsset = 'assets/images/weathers/';
 class AssetCustom{
-  static String getLinkImage (String name) =>'$linkAsset${name.replaceAll(" ", "")}.png';
+  static String getLinkImage (String name) =>'$linkAsset${name.replaceAll(" ", "").toLowerCase()}.png';
 }
 class Mykey{
   static const String api_token ="f33e152fe62a30f1f848f9d8b1b9e948";
